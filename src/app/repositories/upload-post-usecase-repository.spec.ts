22edee6f9c2b1f896383d34b512f007620ae39fb
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-04-28 21:06:25 +0900

    ng g class repositories/upload-post-usecase-repository

---
+++ src/app/repositories/upload-post-usecase-repository.spec.ts
@@ -0,0 +1,7 @@
+import { UploadPostUsecaseRepository } from './upload-post-usecase-repository';
+
+describe('UploadPostUsecaseRepository', () => {
+  it('should create an instance', () => {
+    expect(new UploadPostUsecaseRepository()).toBeTruthy();
+  });
+});
