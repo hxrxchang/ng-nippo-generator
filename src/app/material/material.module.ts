b430df6928089718e576dbc6816d4d60469df6b9
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-04-29 10:03:16 +0900

    set header

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material';
+
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [MatToolbarModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
