86b758bd2c0236f001ca0c9b8e87220de9c847fd
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-05-02 21:17:16 +0900

    install MatInputModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatToolbarModule],
+  exports: [MatToolbarModule, MatInputModule],
 })
 export class MaterialModule {}
