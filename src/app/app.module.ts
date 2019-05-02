27ba41c6c63a662a7726c973e61070d7bd3f589a
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-05-02 20:58:02 +0900

    ng g c components/upload-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadComponent } from './pages/upload/upload.component';
-import { MaterialModule } from './material/material.module';
+import { UploadFormComponent } from './components/upload-form/upload-form.component';
 
 @NgModule({
-  declarations: [AppComponent, UploadComponent],
+  declarations: [AppComponent, UploadComponent, UploadFormComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
