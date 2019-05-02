7030a031dd7e3faaf00dcbdc129d9b62b2c1c382
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-05-03 02:53:40 +0900

    set button and modified style

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatToolbarModule, MatInputModule } from '@angular/material';
+import { MatToolbarModule, MatInputModule, MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatToolbarModule, MatInputModule],
+  exports: [MatToolbarModule, MatInputModule, MatButtonModule, MatIconModule],
 })
 export class MaterialModule {}
