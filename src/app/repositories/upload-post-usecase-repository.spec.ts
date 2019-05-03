23524ecf004ef6fc9a7167a0f9d12985ce32d665
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-05-03 18:26:19 +0900

    ng g class repositories/upload-report-repository

--- src/app/repositories/upload-post-usecase-repository.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UploadPostUsecaseRepository } from './upload-post-usecase-repository';
-
-describe('UploadPostUsecaseRepository', () => {
-  it('should create an instance', () => {
-    expect(new UploadPostUsecaseRepository()).toBeTruthy();
-  });
-});
---
+++ src/app/repositories/upload-report-repository.spec.ts
@@ -0,0 +1,7 @@
+import { UploadReportRepository } from './upload-report-repository';
+
+describe('UploadReportRepository', () => {
+  it('should create an instance', () => {
+    expect(new UploadReportRepository()).toBeTruthy();
+  });
+});
