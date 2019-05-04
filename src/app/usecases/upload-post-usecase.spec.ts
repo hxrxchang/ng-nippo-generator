a9c059cff98d2625592a2ad94ecde49645710e99
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-05-04 21:32:50 +0900

    add test

--- src/app/usecases/upload-post-usecase.spec.ts
+++ src/app/usecases/upload-post-usecase.spec.ts
@@ -1,18 +1,28 @@
 import { UploadPostUsecase } from './upload-post-usecase';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { UploadReportRepository } from '../repositories/upload-report-repository';
 
 describe('UploadPostUsecase', () => {
   let usecase: UploadPostUsecase;
+  let repository: UploadReportRepository;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
     });
     usecase = TestBed.get(UploadPostUsecase);
+    repository = TestBed.get(UploadReportRepository);
   });
 
   it('should create an instance', () => {
     expect(usecase).toBeTruthy();
+  });
+
+  it('call uploadReport() method', () => {
+    const markdown = '# this is test';
+    spyOn(repository, 'post');
+    usecase.uploadReport(markdown);
+    expect(repository.post).toHaveBeenCalledWith(markdown);
   });
 });
