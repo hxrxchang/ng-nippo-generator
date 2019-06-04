60dc69a44dde1c7376ccbef0465e43233fc79888
hxrxchang <m.y.y.0326.cg@gmail.com>
2019-06-04 09:47:05 +0900

    yarn ng update @angular/material

--- src/app/components/dialog/dialog.component.spec.ts
+++ src/app/components/dialog/dialog.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DialogComponent } from './dialog.component';
 import { MaterialModule } from 'src/app/material/material.module';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('DialogComponent', () => {
   let component: DialogComponent;
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatToolbarModule, MatInputModule, MatButtonModule, MatIconModule, MatSnackBarModule, MatDialogModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
